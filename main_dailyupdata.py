519e63b4d88809aecab77e891a0efe4f80b2b042
StudentZZZ-CQF <69203465+StudentZZZ-CQF@users.noreply.github.com>
2021-01-23 15:04:53 -0500

    Create main_dailyupdata.py

---
+++ main_dailyupdata.py
@@ -0,0 +1,31 @@
+#Daily update creat tables
+import finnhub
+import pandas as pd
+import psycopg2
+import sqlalchemy
+import numpy as np
+import os 
+import os.path
+import connect_db
+import Rdate
+import DBtable
+from datetime import datetime
+from dotenv import load_dirname
+
+
+#download the SP500 stocks as taget 
+table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+SP500_NAME=table[0]['Symbol']
+Table_N=pd.read_csv('tablename.csv')
+
+#connect to database
+engine=connect_to_db
+cur=engine.cursor()
+enddate=int(datetime.timestamp(time))
+for i in range(100):
+    engine=connect_to_db()
+    cur=engine.cursor()
+    Table_N[i]=DBtable.creat_dailysqltable(cur,engine,SP500_NAME[i])
+    df_D=Rdate.df_finhub(SP500_NAME[i],'D',enddate-31622400,enddate)
+    uploaddata_daily(cur,Table_N[i][i],df_D)
+    
