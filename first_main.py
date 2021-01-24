aacf7f9f5dee4e1c755573c0279c5a377e27bc2e
StudentZZZ-CQF <69203465+StudentZZZ-CQF@users.noreply.github.com>
2021-01-23 21:38:12 -0500

    Update first_main.py

--- first_main.py
+++ first_main.py
@@ -16,8 +16,8 @@ from dotenv import load_dirname
 #download the SP500 stocks as taget 
 table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
 SP500_NAME=table[0]['Symbol']
-SP500_NAME=table[0]['Symbol']
-Table_N=table[0]['Symbol']
+Table_ND=table[0]['Symbol']
+Table_NM=table[0]['Symbol']
 
 #connect to database
 engine=connect_to_db
@@ -26,8 +26,16 @@ enddate=int(datetime.timestamp(time))
 for i in range(100):
     engine=connect_to_db()
     cur=engine.cursor()
-    Table_N[i]=DBtable.creat_dailysqltable(cur,engine,SP500_NAME[i])
+    Table_ND[i]=DBtable.creat_dailysqltable(cur,engine,SP500_NAME[i])
     df_D=Rdate.df_finhub(SP500_NAME[i],'D',enddate-31622400,enddate)
-    uploaddata_daily(cur,Table_N[i][i],df_D)
-
-Table_N.to_csv('tablename.csv')
+    uploaddata_daily(cur,Table_ND[i][i],df_D)
+    
+for i in range(100):
+    engine=connect_to_db()
+    cur=engine.cursor()
+    Table_NM[i]=DBtable.creat_dailysqltable(cur,engine,SP500_NAME[i])
+    df_D=Rdate.df_finhub(SP500_NAME[i],'D',enddate-31622400,enddate)
+    uploaddata_daily(cur,Table_NM[i][i],df_D)
+    
+Table_ND.to_csv('tablenameD.csv')
+Table_NM.to_csv('tablenameM.csv')
