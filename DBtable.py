431eb1fbba349609e6b41ed0b5592de5656d3775
StudentZZZ-CQF <69203465+StudentZZZ-CQF@users.noreply.github.com>
2021-01-23 21:35:51 -0500

    Update DBtable.py

--- DBtable.py
+++ DBtable.py
@@ -1,15 +1,22 @@
 def creat_dailysqltable(cur,engine,Sysblem):
-    name_Table="DailyD"+Sysblem
-    sqlCreateTable ="create table "+name_Table+" (close_price FLOAT, high_price FLOAT, low_price FLOAT, open_price FLOAT,s varchar(255),t INT,volume INT);"
+    name_TableD="DailyD"+Sysblem
+    sqlCreateTable ="create table "+name_TableD+" (close_price FLOAT, high_price FLOAT, low_price FLOAT, open_price FLOAT,s varchar(255),t INT,volume INT);"
     cur.execute(sqlCreateTable)
     engine.commit()
-    return name_Table
+    return name_TableD
+
+def creat_onemsqltable(cur,engine,Sysblem):
+    name_TableM="DailyM"+Sysblem
+    sqlCreateTable ="create table "+name_TableM+" (close_price FLOAT, high_price FLOAT, low_price FLOAT, open_price FLOAT,s varchar(255),t INT,volume INT);"
+    cur.execute(sqlCreateTable)
+    engine.commit()
+    return name_TableM
 
 
-def uploaddata_daily(cur,name_Table,df_D):
+def uploaddata_daily(cur,name_TableD,df_D):
     cols = ",".join([str(i) for i in df_D.columns.tolist()])
-    table_name='DailyD'
+    table_name=name_TableD
     # Insert DataFrame recrds one by one.
     for i,row in df_D.iterrows():
-        sql = "INSERT INTO " +table_name+" (" +cols + ") VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        sql = "INSERT INTO " +name_TableD+" (" +cols + ") VALUES (" + "%s,"*(len(row)-1) + "%s)"
         cur.execute(sql, tuple(row))
